7f46ff5285ea8bd099d1524ce73f2c1cdaef46ce
misha-2310 <mishapat2000@gmail.com>
2022-06-19 23:20:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean result1 = TeenNumberChecker.hasTeen(9,10, 11);
+        System.out.println(result1);
+
+        boolean result = TeenNumberChecker.isTeen(20);
+        System.out.println(result);
+    }
+}
